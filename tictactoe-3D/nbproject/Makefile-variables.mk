3820971ce302e21307254a2ae26adbda2106d51c
Roy Abou Samra <67294748+royabousamra@users.noreply.github.com>
2020-06-24 15:41:53 +0300

    Add files via upload

---
+++ tictactoe-3D/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=tictactoe-3d-project
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/tictactoe-3d-project
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=tictactoe-3d-project.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/tictactoe-3d-project.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=tictactoe-3d-project
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/tictactoe-3d-project
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=tictactoe-3d-project.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/tictactoe-3d-project.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
